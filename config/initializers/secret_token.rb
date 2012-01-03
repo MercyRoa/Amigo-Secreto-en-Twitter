abc5157ad97c1ecf206372e6a3cceac8ed3af72f
Mercy Roa <mercy@grupoinnovait.com>
2012-01-03 17:34:48 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Secretfriend::Application.config.secret_token = 'd6103a064fdf348fb367441511b31168fc51ac1c0f6b9228b394e133c679fda01370092b35787f206753d10fef02a65adc82f639fc909f30a2a67ced51379e16'
